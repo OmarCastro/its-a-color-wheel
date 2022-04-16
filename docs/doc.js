f9ec82969550da900fdab4c43b41b517a7155dd5
Omar Castro <omar.castro.360@gmail.com>
2022-04-16 14:09:51 +0100

    doc: improve documentation

--- docs/doc.js
+++ docs/doc.js
@@ -13,3 +13,28 @@ document.body.addEventListener("input", (event) => {
   const newText = componentName.textContent
   document.body.querySelectorAll(".component-name-ref").forEach(ref => ref.textContent = newText)
 })
+
+
+
+
+const exampleCode =  (strings, ...expr) => {
+  let statement = strings[0]
+
+  for(let i = 0; i < expr.length; i++){
+      statement += expr[i].replace(/</g, "&lt")
+      .replaceAll("{{elementName}}", '<span class="component-name-ref">color-wheel</span>')
+      .replaceAll("{{mobile:ui-mode}}", '<span contenteditable="true" class="ui-mode-ref">mobile</span>')
+      statement += strings[i+1]
+  }
+
+  return statement
+}
+
+document.body.querySelectorAll("script.html-example").forEach(element => {
+  element.outerHTML = exampleCode`<pre><code class="language-markup">${element.innerHTML}</code></pre>`
+})
+
+document.body.querySelectorAll("script.css-example").forEach(element => {
+  element.outerHTML = exampleCode`<pre><code class="language-css">${element.innerHTML}</code></pre>`
+})
+            
