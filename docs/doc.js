c01a5f01b0bd855812f32efdf28cbcd002973fed
Omar Castro <omar.castro.360@gmail.com>
2022-07-03 21:30:35 +0100

    docs: improve documentation
    - add editable to ui-mode
    
    - add editable to inner-radius
    
    - add editable to lightness
    
    - add editable to value
    
    - refactor css class "inner-radius--none"
    to "inner-radius--custom"

--- docs/doc.js
+++ docs/doc.js
@@ -7,11 +7,56 @@ document.addEventListener("DOMContentLoaded", function () {
       });
 });
 
-document.body.addEventListener("input", (event) => {
+
+const reactElementNameChange = (event) => {
   const componentName = event.target.closest(".component-name-edit")
-  if(componentName == null){ return }
+  if(componentName == null){ return false }
   const newText = componentName.textContent
   document.body.querySelectorAll(".component-name-ref").forEach(ref => ref.textContent = newText)
+  return true
+}
+
+
+const reactUIModeChange = (event) => {
+  const uiMode = event.target.closest(".example .ui-mode-edit")
+  if(uiMode == null){ return false }
+  const uiModeText = uiMode.textContent
+  const example = uiMode.closest(".example")
+  example.querySelectorAll("color-wheel").forEach(ref => ref.style.setProperty("--ui-mode", `"${uiModeText}"`))
+  return true
+}
+
+
+const reactInnerRadiusChange = (event) => {
+  const innerRadius = event.target.closest(".example .inner-radius-edit")
+  if(innerRadius == null){ return false }
+  const innerRadiusText = innerRadius.textContent
+  const example = innerRadius.closest(".example")
+  example.querySelectorAll("color-wheel").forEach(ref => ref.style.setProperty("--color-wheel--inner-radius", `${innerRadiusText}%`))
+  return true
+}
+
+const reactLightnessChange = (event) => {
+  const lightness = event.target.closest(".example .lightness-edit")
+  if(lightness == null){ return false }
+  const lightnessText = lightness.textContent
+  const example = lightness.closest(".example")
+  example.querySelectorAll("color-wheel").forEach(ref => ref.setAttribute("lightness", lightnessText))
+  return true
+}
+
+const reactValueChange = (event) => {
+  const value = event.target.closest(".example .value-edit")
+  if(value == null){ return false }
+  const valueText = value.textContent
+  const example = value.closest(".example")
+  example.querySelectorAll("color-wheel").forEach(ref => ref.setAttribute("value", valueText))
+  return true
+}
+
+
+document.body.addEventListener("input", (event) => {
+  reactElementNameChange(event) || reactUIModeChange(event) || reactLightnessChange(event) || reactValueChange(event)
 })
 
 
@@ -23,7 +68,10 @@ const exampleCode =  (strings, ...expr) => {
   for(let i = 0; i < expr.length; i++){
       statement += expr[i].replace(/</g, "&lt")
       .replaceAll("{{elementName}}", '<span class="component-name-ref">color-wheel</span>')
-      .replaceAll("{{mobile:ui-mode}}", '<span contenteditable="true" class="ui-mode-ref">mobile</span>')
+      .replace(/{{([^:]+):ui-mode}}/, '<span contenteditable="true" class="ui-mode-edit">$1</span>')
+      .replace(/{{([^:]+):inner-radius}}/, '<span contenteditable="true" class="inner-radius-edit">$1</span>')
+      .replace(/{{([^:]+):lightness}}/, '<span contenteditable="true" class="lightness-edit">$1</span>')
+      .replace(/{{([^:]+):value}}/, '<span contenteditable="true" class="value-edit">$1</span>')
       statement += strings[i+1]
   }
 
