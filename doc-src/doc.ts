ffc8ddf4221850aadcae30af0a6855bed0b3aa12
Omar Castro <omar.castro.360@gmail.com>
2023-01-21 13:36:15 +0000

    src: create a doc-src to compile documentation
    - create a doc-src to compile documentation
    
    - use it to import dependencies using deno

--- doc-src/doc.ts
+++ doc-src/doc.ts
@@ -1,3 +1,7 @@
+import dedent from './dedent.util.ts';
+import 'https://esm.sh/v103/prismjs@1.29.0'
+import '../src/color-wheel.element.js?named=color-wheel'
+
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll('pre code').forEach((el) => {
         const html = el.innerHTML
@@ -8,8 +12,8 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-const reactElementNameChange = (event) => {
-  const componentName = event.target.closest(".component-name-edit")
+const reactElementNameChange = (event: Event) => {
+  const componentName = (event.target as HTMLElement).closest(".component-name-edit") as HTMLElement
   if(componentName == null){ return false }
   const newText = componentName.textContent
   document.body.querySelectorAll(".component-name-ref").forEach(ref => ref.textContent = newText)
@@ -17,40 +21,40 @@ const reactElementNameChange = (event) => {
 }
 
 
-const reactUIModeChange = (event) => {
-  const uiMode = event.target.closest(".example .ui-mode-edit")
+const reactUIModeChange = (event: Event) => {
+  const uiMode = (event.target as HTMLElement).closest(".example .ui-mode-edit") as HTMLElement
   if(uiMode == null){ return false }
   const uiModeText = uiMode.textContent
   const example = uiMode.closest(".example")
-  example.querySelectorAll("color-wheel").forEach(ref => ref.style.setProperty("--ui-mode", `"${uiModeText}"`))
+  example?.querySelectorAll<HTMLElement>("color-wheel").forEach(ref => ref.style.setProperty("--ui-mode", `"${uiModeText}"`))
   return true
 }
 
 
-const reactInnerRadiusChange = (event) => {
-  const innerRadius = event.target.closest(".example .inner-radius-edit")
+const reactInnerRadiusChange = (event: Event) => {
+  const innerRadius = (event.target as HTMLElement).closest(".example .inner-radius-edit")  as HTMLElement
   if(innerRadius == null){ return false }
   const innerRadiusText = innerRadius.textContent
   const example = innerRadius.closest(".example")
-  example.querySelectorAll("color-wheel").forEach(ref => ref.style.setProperty("--color-wheel--inner-radius", `${innerRadiusText}%`))
+  example?.querySelectorAll<HTMLElement>("color-wheel").forEach(ref => ref.style.setProperty("--color-wheel--inner-radius", `${innerRadiusText}%`))
   return true
 }
 
-const reactLightnessChange = (event) => {
-  const lightness = event.target.closest(".example .lightness-edit")
+const reactLightnessChange = (event: Event) => {
+  const lightness = (event.target as HTMLElement).closest(".example .lightness-edit")as HTMLElement
   if(lightness == null){ return false }
   const lightnessText = lightness.textContent
   const example = lightness.closest(".example")
-  example.querySelectorAll("color-wheel").forEach(ref => ref.setAttribute("lightness", lightnessText))
+  example?.querySelectorAll<HTMLElement>("color-wheel").forEach(ref => ref.setAttribute("lightness", lightnessText?? ""))
   return true
 }
 
-const reactValueChange = (event) => {
-  const value = event.target.closest(".example .value-edit")
+const reactValueChange = (event: Event) => {
+  const value = (event.target as HTMLElement).closest(".example .value-edit")as HTMLElement
   if(value == null){ return false }
   const valueText = value.textContent
   const example = value.closest(".example")
-  example.querySelectorAll("color-wheel").forEach(ref => ref.setAttribute("value", valueText))
+  example?.querySelectorAll<HTMLElement>("color-wheel").forEach(ref => ref.setAttribute("value", valueText ?? ""))
   return true
 }
 
@@ -62,11 +66,13 @@ document.body.addEventListener("input", (event) => {
 
 
 
-const exampleCode =  (strings, ...expr) => {
-  let statement = strings[0]
+const exampleCode =  (strings:  TemplateStringsArray, ...expr: unknown[]) => {
+
+  let statement = strings[0];
+
 
   for(let i = 0; i < expr.length; i++){
-      statement += expr[i].replace(/</g, "&lt")
+      statement += String(expr[i]).replace(/</g, "&lt")
       .replaceAll("{{elementName}}", '<span class="component-name-ref">color-wheel</span>')
       .replace(/{{([^:]+):ui-mode}}/, '<span contenteditable="true" class="ui-mode-edit">$1</span>')
       .replace(/{{([^:]+):inner-radius}}/, '<span contenteditable="true" class="inner-radius-edit">$1</span>')
@@ -79,10 +85,10 @@ const exampleCode =  (strings, ...expr) => {
 }
 
 document.body.querySelectorAll("script.html-example").forEach(element => {
-  element.outerHTML = exampleCode`<pre><code class="language-markup">${element.innerHTML}</code></pre>`
+  element.outerHTML = exampleCode`<pre><code class="language-markup keep-markup">${dedent(element.innerHTML)}</code></pre>`
 })
 
 document.body.querySelectorAll("script.css-example").forEach(element => {
-  element.outerHTML = exampleCode`<pre><code class="language-css">${element.innerHTML}</code></pre>`
+  element.outerHTML = exampleCode`<pre><code class="language-css keep-markup">${dedent(element.innerHTML)}</code></pre>`
 })
-            
+
