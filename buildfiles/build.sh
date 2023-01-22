2f40dadf2a1d1796dd647759d21a0d570756311e
Omar Castro <omar.castro.360@gmail.com>
2023-01-22 22:51:56 +0000

    src: add deno.jsonc config

--- buildfiles/build.sh
+++ buildfiles/build.sh
@@ -1,6 +1,6 @@
 
 # build doumentation
-deno bundle --config tsconfig.json doc-src/doc.ts > docs/doc.js
+deno bundle --config deno.jsonc doc-src/doc.ts > docs/doc.js
 cp doc-src/doc.css docs/doc.css
 rm -r docs/src
 cp -r src docs/src
