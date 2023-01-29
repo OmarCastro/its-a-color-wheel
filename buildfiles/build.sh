1a6cbb30ab797f3da4ff8521fcd711da7e02bbc5
Omar Castro <omar.castro.360@gmail.com>
2023-01-29 01:42:02 +0000

    build: use esbuild to bundle docs css

--- buildfiles/build.sh
+++ buildfiles/build.sh
@@ -8,6 +8,6 @@ rm -rf build
 # build doumentation
 mkdir -p build/docs
 deno bundle --config deno.jsonc docs/doc.ts > build/docs/doc.js
-cp docs/doc.css build/docs/doc.css
+npx esbuild docs/doc.css --bundle --minify --sourcemap --outfile=build/docs/doc.css
 cp docs/index.html build/docs/index.html
 cp -r src build/docs/src
