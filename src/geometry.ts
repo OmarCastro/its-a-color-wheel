15976afe39d79d1dbfc320da3c706595292039cc
Omar Castro <omar.castro.360@gmail.com>
2023-01-30 01:29:18 +0000

    test: add tests to geometry

--- src/geometry.ts
+++ src/geometry.ts
@@ -1,34 +1,30 @@
+
+interface Rect {
+    x: number
+    y: number
+    width: number
+    height: number
+}
+
 /**
  * @param {{x: number, y: number}} p1
  * @param {{x: number, y: number}} p2
  */
 export const calculateDistanceBetween2Points = ({x: x1, y: y1}, {x: x2, y: y2}) => Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
 
-
 export const CircleInfo = {
-    /**
-     * @param {DOMRect} rect 
-     * @param {number} innerRadiusPerc 
-     * @returns 
-     */
-    fromRectWithPercentInnerRadius(rect, innerRadiusPerc){
+    fromRectWithPercentInnerRadius(rect: Rect, innerRadiusPerc: number){
         const {x, y, width, height} = rect
         const radius = Math.min(width, height) / 2
         const innerRadius = (innerRadiusPerc * 0.01) * radius
-        const center = new DOMPointReadOnly(x + width/2 + y + height/2)
+        const center = {x: x + width/2, y: y + height/2}
         return { innerRadiusPerc, radius, innerRadius, center }
     },
-
-    /**
-     * @param {DOMRect} rect 
-     * @param {number} innerRadius 
-     * @returns 
-     */
-    fromRectWithInnerRadius(rect, innerRadius){
+    fromRectWithInnerRadius(rect:Rect, innerRadius: number){
         const {x, y, width, height} = rect
         const radius = Math.min(width, height) / 2
         const innerRadiusPerc = innerRadius * 100 / radius
-        const center = new DOMPointReadOnly(x + width/2 + y + height/2)
+        const center = {x: x + width/2, y: y + height/2}
         return { innerRadiusPerc, radius, innerRadius, center }
     }
 }
