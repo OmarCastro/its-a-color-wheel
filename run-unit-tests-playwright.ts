043c472dc7b7c2f023cad3db7307acf43045dad1
Omar Castro <omar.castro.360@gmail.com>
2023-02-02 03:43:58 +0000

    test: simplify run unit tests script

--- run-unit-tests-playwright.ts
+++
@@ -1,5 +0,0 @@
-import "./test-utils/unit/unit-test.provider"
-import glob from "glob"
-
-glob("src/**/*.unit.spec.js", {})
-
--- run-unit-tests.ts
+++ run-unit-tests.ts
@@ -1,7 +1,1 @@
-import { globToRegExp } from "https://deno.land/std@0.175.0/path/mod.ts";
-import { walk } from "https://deno.land/std@0.175.0/fs/walk.ts";
-
-for await (const dir of walk("src", {match: [globToRegExp("**/*.spec.ts")], includeDirs:false})){
-    console.log("importing "+dir.path)
-    await import("./"+dir.path)
-}
+import "./src/geometry.unit.spec.ts"
