d256cc029777ca9500ba99658f2c56d121e27382
Omar Castro <omar.castro.360@gmail.com>
2023-02-05 13:12:29 +0000

    test: add drag and drop test

--- src/color-wheel.element.ui.spec.ts
+++ src/color-wheel.element.ui.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '../test-utils/ui-tests.ts'
 
-test('base test', async ({ page }) => {
+test('base test visual testing', async ({ page }) => {
   await page.goto('./build/docs/test-page.html');
   
   const colorWheel = page.locator('.test--base .test__view color-wheel')
@@ -9,7 +9,7 @@ test('base test', async ({ page }) => {
 });
 
 
-test('lightness', async ({ page }) => {
+test('lightness visual testing', async ({ page }) => {
   await page.goto('./build/docs/test-page.html');
     
   const colorWheel = page.locator('.test--lightness .test__view color-wheel')
@@ -19,7 +19,7 @@ test('lightness', async ({ page }) => {
   await expect.soft(await colorWheel.screenshot()).toMatchSnapshot('color-wheel-lightness-75.png');
 });
 
-test('value', async ({ page }) => {
+test('value visual testing', async ({ page }) => {
   await page.goto('./build/docs/test-page.html');
     
   const colorWheel = page.locator('.test--value .test__view color-wheel')
@@ -30,4 +30,28 @@ test('value', async ({ page }) => {
   await colorWheel.evaluate(node => node.setAttribute("value", "100"))
   await expect.soft(await colorWheel.screenshot()).toMatchSnapshot('color-wheel-value-100.png');
 
+});
+
+
+test('dragging pointer should', async ({ page }) => {
+  await page.goto('./build/docs/test-page.html');
+    
+  const colorWheel = page.locator('.test--base .test__view color-wheel');
+  const slider = colorWheel.getByRole("slider")
+  const sliderBoundingBox = await slider.boundingBox();
+  const colorWheelBoundingBox = await colorWheel.boundingBox();
+  test.fail(!sliderBoundingBox || !colorWheelBoundingBox, "bounding box is null")
+  if(!sliderBoundingBox || !colorWheelBoundingBox){
+    throw new Error("Should have failed already") 
+    //also, the if condition stops TS complaining that boundingBox is possibly null 
+  }
+  const sliderCenterPoint = {
+    x: sliderBoundingBox.x + sliderBoundingBox.width  / 2,
+    y: sliderBoundingBox.y + sliderBoundingBox.height / 2
+  }
+  await page.mouse.move(sliderCenterPoint.x, sliderCenterPoint.y)
+  await page.mouse.down()
+  await page.mouse.move(sliderCenterPoint.x, colorWheelBoundingBox.y)
+  await page.mouse.up()
+  expect(await colorWheel.evaluate(node => node.getAttribute("saturation"))).toEqual("100");
 });
