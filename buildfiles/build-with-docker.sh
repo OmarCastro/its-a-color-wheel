09ceeb50fad3cfaa9b6c142f607ab9d4700e33c8
Omar Castro <omar.castro.360@gmail.com>
2023-03-03 23:54:04 +0000

    build: add build-with-docker script

---
+++ buildfiles/build-with-docker.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR"/..
+
+sudo docker run --user `id -u`:`id -g` -v `pwd`:/home/pwuser/project  mcr.microsoft.com/playwright:v1.31.2-focal /home/pwuser/project/buildfiles/build.sh
