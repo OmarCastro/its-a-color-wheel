fc586634e52d7992a2e8463a56e4928f7ba70dde
OmarCastro <omar.castro.360@gmail.com>
2025-05-01 22:50:33 +0100

    test: add js setters reflecting changes too

--- src/color-wheel.element.ui.spec.js
+++ src/color-wheel.element.ui.spec.js
@@ -14,7 +14,10 @@ test('lightness visual testing', async ({ page }) => {
   const colorWheel = page.locator('.test--lightness .test__view color-wheel')
 
   await expect.soft(await colorWheel.screenshot()).toMatchSnapshot('color-wheel-lightness-50.png')
-  await colorWheel.evaluate(node => node.setAttribute('lightness', '75'))
+  await colorWheel.evaluate(node => node.setAttribute('lightness', '60'))
+  await expect.soft(await colorWheel.screenshot()).toMatchSnapshot('color-wheel-lightness-60.png')
+  await colorWheel.evaluate(node => { node.lightness = 75 })
+  await expect.soft(colorWheel).toHaveAttribute('lightness', '75')
   await expect.soft(await colorWheel.screenshot()).toMatchSnapshot('color-wheel-lightness-75.png')
 })
 
@@ -26,7 +29,8 @@ test('value visual testing', async ({ page }) => {
   await expect.soft(await colorWheel.screenshot()).toMatchSnapshot('color-wheel-value-50.png')
   await colorWheel.evaluate(node => node.setAttribute('value', '75'))
   await expect.soft(await colorWheel.screenshot()).toMatchSnapshot('color-wheel-value-75.png')
-  await colorWheel.evaluate(node => node.setAttribute('value', '100'))
+  await colorWheel.evaluate(node => { node.value = 100 })
+  await expect.soft(colorWheel).toHaveAttribute('value', '100')
   await expect.soft(await colorWheel.screenshot()).toMatchSnapshot('color-wheel-value-100.png')
 })
 
@@ -150,12 +154,42 @@ test('slider should follow mouse vertical position when dragging the mouse in mo
     y: sliderBoundingBox.y + sliderBoundingBox.height / 2
   }
 
-  const getSturation = async () => await colorWheelElement.evaluate(node => parseInt(node.getAttribute('saturation') ?? '0'))
+  const getSaturation = async () => await colorWheelElement.evaluate(node => parseInt(node.getAttribute('saturation') ?? '0'))
 
-  expect.soft(await getSturation()).toEqual(0)
+  expect.soft(await getSaturation()).toEqual(0)
   await page.mouse.move(sliderCenterPoint.x, sliderCenterPoint.y)
   await page.mouse.down()
   await page.mouse.move(sliderCenterPoint.x, colorWheelBoundingBox.y)
-  expect.soft(await getSturation()).toEqual(100)
+  expect.soft(await getSaturation()).toEqual(100)
+  await page.mouse.up()
+})
+
+test('slider should follow mouse vertical position when dragging the mouse in mobile ui mode with absolute --color-wheel-inner-radius value', async ({ page }) => {
+  await page.goto('./build/docs/test-page.html')
+
+  const colorWheelElement = page.locator('.test--mobile-ui-absolute-inner-radius .test__view color-wheel')
+  await colorWheelElement.scrollIntoViewIfNeeded()
+  const colorWheel = colorWheelElement.locator('.container')
+  const slider = colorWheelElement.getByRole('slider')
+  const sliderBoundingBox = await slider.boundingBox()
+  const colorWheelBoundingBox = await colorWheel.boundingBox()
+  test.fail(!sliderBoundingBox || !colorWheelBoundingBox, 'bounding box is null')
+  if (!sliderBoundingBox || !colorWheelBoundingBox) {
+    throw new Error('Should have failed already')
+    // also, the if condition stops TS complaining that boundingBox is possibly null
+  }
+
+  const sliderCenterPoint = {
+    x: sliderBoundingBox.x + sliderBoundingBox.width / 2,
+    y: sliderBoundingBox.y + sliderBoundingBox.height / 2
+  }
+
+  const getSaturation = async () => await colorWheelElement.evaluate(node => parseInt(node.getAttribute('saturation') ?? '0'))
+
+  expect.soft(await getSaturation()).toEqual(0)
+  await page.mouse.move(sliderCenterPoint.x, sliderCenterPoint.y)
+  await page.mouse.down()
+  await page.mouse.move(sliderCenterPoint.x, colorWheelBoundingBox.y)
+  expect.soft(await getSaturation()).toEqual(100)
   await page.mouse.up()
 })
